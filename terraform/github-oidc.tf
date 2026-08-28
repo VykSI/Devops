7effbdbd1dff301fefcf9e2027ad64cabd614471
VykSI <vishalsmurali@gmail.com>
2026-08-28 17:13:17 +0530

    temp fix

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -48,13 +48,14 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_repository}:environment:${var.environment}"
+        "repo:${var.github_repository}:*"
       ]
     }
+
   }
 }
 
