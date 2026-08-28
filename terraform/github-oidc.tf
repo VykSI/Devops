52cc0d3ed95ba4d3e7d992df6d917fafa5901a68
VykSI <vishalsmurali@gmail.com>
2026-08-28 18:09:53 +0530

    fix describe ecs_task

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -144,7 +144,8 @@ data "aws_iam_policy_document" "github_actions_ecs" {
     ]
 
     resources = [
-      aws_iam_role.ecs_execution.arn
+      aws_iam_role.ecs_execution.arn,
+      aws_iam_role.ecs_task.arn
     ]
   }
 }
