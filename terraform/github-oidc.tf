d32478e3afd7eb5e0e4967d827387d845a5db840
VykSI <vishalsmurali@gmail.com>
2026-08-28 15:55:29 +0530

    oidc change

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -48,12 +48,12 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
 
     condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
+        test     = "StringEquals"
+        variable = "token.actions.githubusercontent.com:sub"
 
-      values = [
-        "repo:${var.github_repository}:ref:refs/heads/main"
-      ]
+        values = [
+            "repo:${var.github_repository}:environment:staging"
+        ]
     }
   }
 }
