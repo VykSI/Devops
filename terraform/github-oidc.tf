7061da7555db3a71f4b9cdcdbb0feb061d08e9cb
VykSI <vishalsmurali@gmail.com>
2026-08-28 17:32:38 +0530

    new github way of oidc

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -52,7 +52,8 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:${var.github_repository}:*"
+        "repo:${var.github_repository}:environment:${var.environment}",
+        "repo:VykSI@*/Devops@*:environment:${var.environment}"
       ]
     }
 
